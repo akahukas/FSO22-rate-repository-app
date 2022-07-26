9a18bcc2db502f7297b4cda0faab761f6c72c8f7
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-07-26 13:52:37 +0300

    Adds The Sign in Form mutation -task. (10.13)

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -6,6 +6,8 @@ import { Pressable, View, StyleSheet } from 'react-native'
 import { Formik } from 'formik'
 import * as yup from 'yup'
 
+import useSignIn from '../hooks/useSignIn'
+
 import theme from '../theme'
 
 const initialValues = {
@@ -71,8 +73,17 @@ const validationSchema = yup.object().shape({
 })
 
 const SignIn = () => {
-  const onSubmit = (values) => {
-    console.log(values)
+  const [signIn] = useSignIn()
+
+  const onSubmit = async (values) => {
+    const { username, password } = values
+    
+    try {
+      const { data } = await signIn({ username, password })
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
